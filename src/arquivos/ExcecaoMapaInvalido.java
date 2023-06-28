5a5be0d3ac3201b2fbdca6501bc67fe953847ba5
unknown <l247064@dac.unicamp.br>
2023-06-27 21:35:48 -0300

    Waranings removidos

--- src/arquivos/ExcecaoMapaInvalido.java
+++ src/arquivos/ExcecaoMapaInvalido.java
@@ -1,6 +1,11 @@
 package arquivos;
 
 public class ExcecaoMapaInvalido extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public ExcecaoMapaInvalido(){
 		super("Arquivo de mapa inválido!");
 	}
